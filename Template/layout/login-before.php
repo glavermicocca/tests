d616d9a706e87185b110d8b382f66b8e3ae2c690
Aljaž "g5pw" Srebrnič <a2piratesoft@gmail.com>
2017-03-11 01:29:53 +0100

    Add blurb and logo to login page

---
+++ Template/layout/login-before.php
@@ -0,0 +1,11 @@
+<div>
+    <img alt="Mittelab Logo" src="/plugins/MittelTheme/Template/img/logo.svg" width="130" style="float: left;"/>
+    <h1>Welcome to Mittelab Tasks!</h1>
+    <p>
+        This is the service used to manage tasks and stuff we do at the space.
+    </p>
+    <p>
+        Members only!
+    </p>
+</div>
+<div style="clear:both;">&nbsp;</div>
