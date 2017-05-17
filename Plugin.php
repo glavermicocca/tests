71ecb883a35dec436287ce89dec84b4b27812d9e
Aljaž "g5pw" Srebrnič <a2piratesoft@gmail.com>
2017-05-17 17:49:25 +0200

    Remove useless template

--- Plugin.php
+++ Plugin.php
@@ -8,7 +8,6 @@ class Plugin extends Base
     public function initialize()
     {
         $this->hook->on('template:layout:css', array('template' => 'plugins/MittelTheme/Template/css/main.css'));
-        # $this->template->hook->attach('template:layout:top', 'MittelTheme:layout/top');
         # $this->template->setTemplateOverride('header', 'mitteltheme:layout/header');
         $this->template->hook->attach('template:auth:login-form:before', 'MittelTheme:layout/login-before');
         $this->template->setTemplateOverride('header/title', 'MittelTheme:layout/title', array(
--- Template/layout/top.php
+++
@@ -1,3 +0,0 @@
-<div id="top-bar">
-    Top Bar
-</div>
